1e4514619016fcd51cd29e19f26272c608751f5d
Ahmed Rashad Jamion <jamionahmedrashadi@gmail.com>
2024-05-19 20:31:34 +0800

    bbnetcalculator

---
+++ Calculator/Calculator.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ButtonSeven = New Button()
+        ButtonEight = New Button()
+        ButtonNine = New Button()
+        ButtonDivide = New Button()
+        ButtonFour = New Button()
+        ButtonFive = New Button()
+        ButtonSix = New Button()
+        ButtonMultiply = New Button()
+        ButtonOne = New Button()
+        ButtonTwo = New Button()
+        ButtonThree = New Button()
+        ButtonSubtract = New Button()
+        ButtonZero = New Button()
+        ButtonPoint = New Button()
+        ButtonClear = New Button()
+        ButtonAdd = New Button()
+        ButtonEquals = New Button()
+        Display = New TextBox()
+        SuspendLayout()
+        ' 
+        ' ButtonSeven
+        ' 
+        ButtonSeven.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonSeven.Location = New Point(17, 91)
+        ButtonSeven.Name = "ButtonSeven"
+        ButtonSeven.Size = New Size(70, 70)
+        ButtonSeven.TabIndex = 0
+        ButtonSeven.Text = "7"
+        ButtonSeven.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonEight
+        ' 
+        ButtonEight.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonEight.Location = New Point(93, 91)
+        ButtonEight.Name = "ButtonEight"
+        ButtonEight.Size = New Size(70, 70)
+        ButtonEight.TabIndex = 0
+        ButtonEight.Text = "8"
+        ButtonEight.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonNine
+        ' 
+        ButtonNine.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonNine.Location = New Point(169, 91)
+        ButtonNine.Name = "ButtonNine"
+        ButtonNine.Size = New Size(70, 70)
+        ButtonNine.TabIndex = 0
+        ButtonNine.Text = "9"
+        ButtonNine.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDivide
+        ' 
+        ButtonDivide.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonDivide.Location = New Point(245, 91)
+        ButtonDivide.Name = "ButtonDivide"
+        ButtonDivide.Size = New Size(70, 70)
+        ButtonDivide.TabIndex = 0
+        ButtonDivide.Text = "÷"
+        ButtonDivide.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonFour
+        ' 
+        ButtonFour.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonFour.Location = New Point(17, 167)
+        ButtonFour.Name = "ButtonFour"
+        ButtonFour.Size = New Size(70, 70)
+        ButtonFour.TabIndex = 0
+        ButtonFour.Text = "4"
+        ButtonFour.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonFive
+        ' 
+        ButtonFive.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonFive.Location = New Point(93, 167)
+        ButtonFive.Name = "ButtonFive"
+        ButtonFive.Size = New Size(70, 70)
+        ButtonFive.TabIndex = 0
+        ButtonFive.Text = "5"
+        ButtonFive.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonSix
+        ' 
+        ButtonSix.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonSix.Location = New Point(169, 167)
+        ButtonSix.Name = "ButtonSix"
+        ButtonSix.Size = New Size(70, 70)
+        ButtonSix.TabIndex = 0
+        ButtonSix.Text = "6"
+        ButtonSix.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonMultiply
+        ' 
+        ButtonMultiply.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonMultiply.Location = New Point(245, 167)
+        ButtonMultiply.Name = "ButtonMultiply"
+        ButtonMultiply.Size = New Size(70, 70)
+        ButtonMultiply.TabIndex = 0
+        ButtonMultiply.Text = "×"
+        ButtonMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonOne
+        ' 
+        ButtonOne.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonOne.Location = New Point(17, 243)
+        ButtonOne.Name = "ButtonOne"
+        ButtonOne.Size = New Size(70, 70)
+        ButtonOne.TabIndex = 0
+        ButtonOne.Text = "1"
+        ButtonOne.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonTwo
+        ' 
+        ButtonTwo.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonTwo.Location = New Point(93, 243)
+        ButtonTwo.Name = "ButtonTwo"
+        ButtonTwo.Size = New Size(70, 70)
+        ButtonTwo.TabIndex = 0
+        ButtonTwo.Text = "2"
+        ButtonTwo.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonThree
+        ' 
+        ButtonThree.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonThree.Location = New Point(169, 243)
+        ButtonThree.Name = "ButtonThree"
+        ButtonThree.Size = New Size(70, 70)
+        ButtonThree.TabIndex = 0
+        ButtonThree.Text = "3"
+        ButtonThree.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonSubtract
+        ' 
+        ButtonSubtract.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonSubtract.Location = New Point(245, 243)
+        ButtonSubtract.Name = "ButtonSubtract"
+        ButtonSubtract.Size = New Size(70, 70)
+        ButtonSubtract.TabIndex = 0
+        ButtonSubtract.Text = "-"
+        ButtonSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonZero
+        ' 
+        ButtonZero.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonZero.Location = New Point(17, 319)
+        ButtonZero.Name = "ButtonZero"
+        ButtonZero.Size = New Size(70, 70)
+        ButtonZero.TabIndex = 0
+        ButtonZero.Text = "0"
+        ButtonZero.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonPoint
+        ' 
+        ButtonPoint.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonPoint.Location = New Point(93, 319)
+        ButtonPoint.Name = "ButtonPoint"
+        ButtonPoint.Size = New Size(70, 70)
+        ButtonPoint.TabIndex = 0
+        ButtonPoint.Text = "."
+        ButtonPoint.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonClear
+        ' 
+        ButtonClear.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonClear.Location = New Point(169, 319)
+        ButtonClear.Name = "ButtonClear"
+        ButtonClear.Size = New Size(70, 70)
+        ButtonClear.TabIndex = 0
+        ButtonClear.Text = "c"
+        ButtonClear.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonAdd
+        ' 
+        ButtonAdd.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonAdd.Location = New Point(245, 319)
+        ButtonAdd.Name = "ButtonAdd"
+        ButtonAdd.Size = New Size(70, 70)
+        ButtonAdd.TabIndex = 0
+        ButtonAdd.Text = "+"
+        ButtonAdd.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonEquals
+        ' 
+        ButtonEquals.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonEquals.Location = New Point(17, 395)
+        ButtonEquals.Name = "ButtonEquals"
+        ButtonEquals.Size = New Size(298, 70)
+        ButtonEquals.TabIndex = 0
+        ButtonEquals.Text = "="
+        ButtonEquals.UseVisualStyleBackColor = True
+        ' 
+        ' Display
+        ' 
+        Display.Font = New Font("Arial Rounded MT Bold", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Display.Location = New Point(17, 23)
+        Display.Name = "Display"
+        Display.Size = New Size(298, 62)
+        Display.TabIndex = 1
+        Display.Text = "0"
+        Display.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(333, 489)
+        Controls.Add(Display)
+        Controls.Add(ButtonAdd)
+        Controls.Add(ButtonClear)
+        Controls.Add(ButtonSubtract)
+        Controls.Add(ButtonThree)
+        Controls.Add(ButtonMultiply)
+        Controls.Add(ButtonPoint)
+        Controls.Add(ButtonSix)
+        Controls.Add(ButtonTwo)
+        Controls.Add(ButtonEquals)
+        Controls.Add(ButtonDivide)
+        Controls.Add(ButtonZero)
+        Controls.Add(ButtonFive)
+        Controls.Add(ButtonOne)
+        Controls.Add(ButtonNine)
+        Controls.Add(ButtonFour)
+        Controls.Add(ButtonEight)
+        Controls.Add(ButtonSeven)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ButtonSeven As Button
+    Friend WithEvents ButtonEight As Button
+    Friend WithEvents ButtonNine As Button
+    Friend WithEvents ButtonDivide As Button
+    Friend WithEvents ButtonFour As Button
+    Friend WithEvents ButtonFive As Button
+    Friend WithEvents ButtonSix As Button
+    Friend WithEvents ButtonMultiply As Button
+    Friend WithEvents ButtonOne As Button
+    Friend WithEvents ButtonTwo As Button
+    Friend WithEvents ButtonThree As Button
+    Friend WithEvents ButtonSubtract As Button
+    Friend WithEvents ButtonZero As Button
+    Friend WithEvents ButtonPoint As Button
+    Friend WithEvents ButtonClear As Button
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents uttonEquals As Button
+    Friend WithEvents Display As TextBox
+    Friend WithEvents ButtonEquals As Button
+
+End Class
---
+++ Calculator/Calculator.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+    Dim FirstNum As Decimal
+    Dim SecondNum As Decimal
+    Dim Operation As Integer
+    Dim OperationSelected As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonOne_Click(sender As Object, e As EventArgs) Handles ButtonOne.Click
+        If Display.Text <> "0" Then
+            Display.Text += "1"
+        Else
+            Display.Text = "1"
+        End If
+    End Sub
+
+    Private Sub ButtonTwo_Click(sender As Object, e As EventArgs) Handles ButtonTwo.Click
+        If Display.Text <> "0" Then
+            Display.Text += "2"
+        Else
+            Display.Text = "2"
+        End If
+    End Sub
+
+    Private Sub ButtonThree_Click(sender As Object, e As EventArgs) Handles ButtonThree.Click
+        If Display.Text <> "0" Then
+            Display.Text += "3"
+        Else
+            Display.Text = "3"
+        End If
+    End Sub
+
+    Private Sub ButtonFour_Click(sender As Object, e As EventArgs) Handles ButtonFour.Click
+        If Display.Text <> "0" Then
+            Display.Text += "4"
+        Else
+            Display.Text = "4"
+        End If
+    End Sub
+
+    Private Sub ButtonFive_Click(sender As Object, e As EventArgs) Handles ButtonFive.Click
+        If Display.Text <> "0" Then
+            Display.Text += "5"
+        Else
+            Display.Text = "5"
+        End If
+    End Sub
+
+    Private Sub ButtonSix_Click(sender As Object, e As EventArgs) Handles ButtonSix.Click
+        If Display.Text <> "0" Then
+            Display.Text += "6"
+        Else
+            Display.Text = "6"
+        End If
+    End Sub
+
+    Private Sub ButtonSeven_Click(sender As Object, e As EventArgs) Handles ButtonSeven.Click
+        If Display.Text <> "0" Then
+            Display.Text += "7"
+        Else
+            Display.Text = "7"
+        End If
+    End Sub
+
+    Private Sub ButtonEight_Click(sender As Object, e As EventArgs) Handles ButtonEight.Click
+        If Display.Text <> "0" Then
+            Display.Text += "8"
+        Else
+            Display.Text = "8"
+        End If
+    End Sub
+
+    Private Sub ButtonNine_Click(sender As Object, e As EventArgs) Handles ButtonNine.Click
+        If Display.Text <> "0" Then
+            Display.Text += "9"
+        Else
+            Display.Text = "9"
+        End If
+    End Sub
+
+    Private Sub ButtonZero_Click(sender As Object, e As EventArgs) Handles ButtonZero.Click
+        If Display.Text <> "0" Then
+            Display.Text += "0"
+        End If
+    End Sub
+
+    Private Sub ButtonPoint_Click(sender As Object, e As EventArgs) Handles ButtonPoint.Click
+        If Not Display.Text.Contains("."c) Then
+            Display.Text += "."
+        End If
+    End Sub
+
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        FirstNum = Display.Text
+        Display.Text = "0"
+        OperationSelected = True
+        Operation = 1 ' addition
+    End Sub
+
+    Private Sub ButtonSubtract_Click(sender As Object, e As EventArgs) Handles ButtonSubtract.Click
+        FirstNum = Display.Text
+        Display.Text = "0"
+        OperationSelected = True
+        Operation = 2 ' subtraction
+    End Sub
+
+    Private Sub ButtonMultiply_Click(sender As Object, e As EventArgs) Handles ButtonMultiply.Click
+        FirstNum = Display.Text
+        Display.Text = "0"
+        OperationSelected = True
+        Operation = 3 ' multiplication
+    End Sub
+
+    Private Sub ButtonDivide_Click(sender As Object, e As EventArgs) Handles ButtonDivide.Click
+        FirstNum = Display.Text
+        Display.Text = "0"
+        OperationSelected = True
+        Operation = 4 ' division
+    End Sub
+
+    Private Sub ButtonEquals_Click(sender As Object, e As EventArgs) Handles ButtonEquals.Click
+        If OperationSelected = True Then
+            SecondNum = Display.Text
+            If Operation = 1 Then
+                Display.Text = FirstNum + SecondNum
+            ElseIf Operation = 2 Then
+                Display.Text = FirstNum - SecondNum
+            ElseIf Operation = 3 Then
+                Display.Text = FirstNum * SecondNum
+            ElseIf Operation = 4 Then
+                If SecondNum = 0 Then
+                    Display.Text = "Error!"
+                Else
+                    Display.Text = FirstNum / SecondNum
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        If Display.Text <> "0" Then
+            Display.Text = "0"
+        End If
+    End Sub
+End Class
